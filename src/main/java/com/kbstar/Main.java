23b3fa7b9ed75a629866b0bdba3e118fdfe95ff2
youngeunkwon <youngnikk4929@gmail.com>
2023-04-17 15:45:21 +0900

    0417_02

--- src/main/java/com/kbstar/Main.java
+++ src/main/java/com/kbstar/Main.java
@@ -6,6 +6,7 @@ public class Main {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
+        int c = 30;
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
