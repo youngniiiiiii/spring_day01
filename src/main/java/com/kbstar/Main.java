ffe7a7b5742174f3773894ae14d64954faf2775e
youngeunkwon <youngnikk4929@gmail.com>
2023-04-17 15:38:31 +0900

    0417zz

--- src/main/java/com/kbstar/Main.java
+++ src/main/java/com/kbstar/Main.java
@@ -4,7 +4,7 @@ package com.kbstar;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-
+        int a = 10;
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
