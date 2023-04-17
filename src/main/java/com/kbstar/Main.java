23ed354d32c7efecc094c9b9735a9760c0790cb7
youngeunkwon <youngnikk4929@gmail.com>
2023-04-17 15:49:26 +0900

    0417_03

--- src/main/java/com/kbstar/Main.java
+++ src/main/java/com/kbstar/Main.java
@@ -7,6 +7,7 @@ public class Main {
         int a = 10;
         int b = 20;
         int c = 30;
+        Service service = new Service();
         for (int i = 1; i <= 5; i++) {
             System.out.println("i = " + i);
         }
---
+++ src/main/java/com/kbstar/Service.java
@@ -0,0 +1,5 @@
+package com.kbstar;
+
+public class Service {
+
+}
